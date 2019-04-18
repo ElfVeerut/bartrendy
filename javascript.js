0d3a37bb026ce933c98645757196bd8b71e14daa
cozmoguard <42993063+cozmoguard@users.noreply.github.com>
2019-04-18 17:37:36 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function NC(){
+//    document.getElementById("Drink1").innerText=drink1;
+//    document.getElementById("Drink2").innerText=drink2;
+//    document.getElementById("Drink3").innerText=drink3;
+//    document.getElementById("Drink4").innerText=drink4;
+//    document.getElementById("Mixer1").innerText=mixer1;
+//    document.getElementById("Mixer2").innerText=mixer2;
+//    document.getElementById("Mixer3").innerText=mixer3;
+    
+    drink1=document.getElementById("Drink1").value;
+    drink2=document.getElementById("Drink2").value;
+    drink3=document.getElementById("Drink3").value;
+    drink4=document.getElementById("Drink4").value;
+    mixer1=document.getElementById("Mixer1").value;
+    mixer2=document.getElementById("Mixer2").value;
+    mixer3=document.getElementById("Mixer3").value;
+    
+    console.log(drink1,drink2,drink3,drink4,mixer1,mixer2,mixer3);}
